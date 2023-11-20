5b119a1eef35adb628e5ec747185dd0a89b82519
Ayotunde <ayopedro2012@gmail.com>
2023-11-20 23:20:02 +0100

    create: auth, messaging, events, user and company modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { EventModule } from './event/event.module';
+import { UserModule } from './user/user.module';
+import { CompanyModule } from './company/company.module';
+import { MessagingModule } from './common/messaging/messaging.module';
+import { PrismaModule } from './common/database/prisma/prisma.module';
+import { PrismaService } from './common/database/prisma/prisma.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    CompanyModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    EventModule,
+    MessagingModule,
+    PrismaModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60,
+          limit: 10,
+        },
+      ],
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
 })
 export class AppModule {}
