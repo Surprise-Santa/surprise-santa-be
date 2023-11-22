c560ea451a057cd8bd247bd5c9daa5acfe3b94b8
Violacordis <ikemviolacordis@gmail.com>
2023-11-22 14:43:28 +0100

    signup implementation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { signupDto } from './dto/signup.dto';
 
+@ApiTags('Authentication')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  async signup(@Body() dto: signupDto) {
+    return this.authService.signup(dto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import * as argon from 'argon2';
+import { signupDto } from './dto/signup.dto';
+import { PrismaService } from '../common/database/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup({ password, ...rest }: signupDto) {
+    const hash = await argon.hash(password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          ...rest,
+          password: hash,
+        },
+      });
+
+      delete user.password;
+
+      return user;
+    } catch (err) {
+      if (err.code === 'P2002') {
+        throw new ForbiddenException('Email address already exists');
+      }
+
+      throw err;
+    }
+  }
+}
