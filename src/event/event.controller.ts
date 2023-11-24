f9ebd4300cb3efd9f6927755d9500753eb7b31ff
Ayotunde <ayopedro2012@gmail.com>
2023-11-24 16:17:36 +0100

    event controller set up

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { EventService } from './event.service';
+import { JwtGuard } from '@@/auth/guard/auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { GetRequestUser } from '@@/common/decorators/get-user.decorator';
+import { User } from '@prisma/client';
+import { CreateEventDto } from './dto/create-event.dto';
+import { AddEventParticipantsDto } from './dto/add-event-participants.dto';
 
+@ApiBearerAuth()
+@UseGuards(JwtGuard)
 @Controller('event')
-export class EventController {}
+export class EventController {
+  constructor(private eventService: EventService) {}
+
+  @Get()
+  async getAllEvents(@GetRequestUser() user: User) {
+    return this.eventService.getEvents(user.id);
+  }
+
+  @Get('/:id')
+  async getEvent(
+    @Param('id', ParseUUIDPipe) id: string,
+    @GetRequestUser() user: User,
+  ) {
+    return this.eventService.getEvent(id, user);
+  }
+
+  @Post('/create')
+  async createEvent(@Body() dto: CreateEventDto, @GetRequestUser() user: User) {
+    return this.eventService.createEvent(dto, user);
+  }
+
+  @Post('/:eventId/add-participants')
+  async addParticipants(
+    @Param('eventId', ParseUUIDPipe) id: string,
+    @Body() dto: AddEventParticipantsDto,
+  ) {
+    return this.eventService.addEventParticipants(id, dto);
+  }
+}
