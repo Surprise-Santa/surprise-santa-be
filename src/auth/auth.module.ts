80c6d7bc4e24e567b8ab1824a893ac7a1de8e55e
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-11-28 07:41:50 +0100

    feat: process emails on queue

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,11 +6,26 @@ import { MailingService } from 'src/common/messaging/mailing/mailing.service';
 import { TokenService } from 'src/common/token/token.service';
 import { CacheModule } from 'src/common/cache/cache.module';
 import { GoogleStrategy } from './strategy/google.strategy';
+import { MessagingQueueProducer } from '../common/messaging/queue/producer';
+import { MessagingQueueConsumer } from '../common/messaging/queue/consumer';
+import { BullModule } from '@nestjs/bull';
+import { QUEUE } from '../common/messaging/interfaces';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy, MailingService, TokenService],
-  imports: [JwtModule.register({}), CacheModule],
+  providers: [
+    AuthService,
+    GoogleStrategy,
+    MailingService,
+    TokenService,
+    MessagingQueueConsumer,
+    MessagingQueueProducer,
+  ],
+  imports: [
+    BullModule.registerQueue({ name: QUEUE }),
+    JwtModule.register({}),
+    CacheModule,
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
