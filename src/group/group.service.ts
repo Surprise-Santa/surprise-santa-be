5f017cf4cc3d34686e1e9f3ae6aee5d05c3924bf
Violacordis <ikemviolacordis@gmail.com>
2023-12-18 22:34:57 +0100

    cleanup

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -81,20 +81,7 @@ export class GroupService {
       },
     });
 
-    const result = groups.map(({ members, ...groupData }) => {
-      const transformedMembers = members.map(({ user, ...member }) => {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { password, ...userWithoutPassword } = user;
-        return { ...member, user: userWithoutPassword };
-      });
-
-      return {
-        ...groupData,
-        members: transformedMembers,
-      };
-    });
-
-    return result;
+    return AppUtilities.removeSensitiveData(groups, 'password', true);
   }
 
   async createGroup(
